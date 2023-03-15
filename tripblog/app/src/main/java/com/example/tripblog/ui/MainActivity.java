b8f8109dfd4455dbb0a9c769b3802401a9b237b5
thong89x <thong89x@gmail.com>
2023-03-15 17:23:57 +0700

    Thong add suggestiong_search_component

--- tripblog/app/src/main/java/com/example/tripblog/ui/MainActivity.java
+++ tripblog/app/src/main/java/com/example/tripblog/ui/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.tripblog.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.example.tripblog.R;
 import com.example.tripblog.databinding.ActivityMainBinding;
 import com.example.tripblog.ui.fragments.HomeFragment;
 import com.example.tripblog.ui.fragments.CreateFragment;
 import com.example.tripblog.ui.fragments.ProfileFragment;
+import com.example.tripblog.ui.post.ViewPost;
+import com.example.tripblog.ui.search.Search;
 
 public class MainActivity extends AppCompatActivity implements MainCallbacks{
 
     ActivityMainBinding binding;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +48,32 @@ public class MainActivity extends AppCompatActivity implements MainCallbacks{
             }
             return true;
         });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Intent intent=new Intent(MainActivity.this, Search.class);
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivityForResult(intent, 1122);
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_trip_and_location, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void replaceFragment(Fragment fragment) {

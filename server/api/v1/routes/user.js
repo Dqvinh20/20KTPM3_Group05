50ffb231357997f6127e7d141de98347df6a3db2
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-04-17 18:42:14 +0700

    Check username exists

--- server/api/v1/routes/user.js
+++ server/api/v1/routes/user.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+
 const UserController = require("../controllers/user.controller");
+const UserService = require("../services/user.service");
+
 const multer = require("../utils/multer");
+const Validation = require("../utils/validation");
+
 router.get("/:user_id/followers", UserController.getFollowers);
 router.get("/:user_id/followings", UserController.getFollowings);
 router.get("/:user_id", UserController.getUserInfo);
@@ -9,7 +15,17 @@ router.post("/:user_id/follow", UserController.followUser);
 router.delete("/:user_id/unfollow", UserController.unfollowUser);
 router.post("/like/:post_id", UserController.likePost);
 router.delete("/unlike/:post_id", UserController.unlikePost);
-router.patch("/update", multer.single("avatar_img"), UserController.updateUser);
+router.patch(
+  "/update",
+  multer.single("avatar_img"),
+  body("user_name").custom((value) => {
+    if (UserService.getUserByUsername(value)) {
+      return Promise.reject("Username already exists");
+    }
+  }),
+  Validation.validate,
+  UserController.updateUser
+);
 module.exports = router;
 
 // TODO:
